d6a07f98ac97a47bc0d172b2ad6b1c3fdb11575e
Gurtej Kanwar <looterguf@HelloThere.(none)>
2012-01-14 16:53:26 -0500

    Comments up all the code

--- src/blargh/__init__.py
+++ src/blargh/__init__.py
@@ -1,3 +1,6 @@
+# Blarghs are classes that define a step function as seen below. They are
+# designed to be able to chain together so that they can start with some input
+# and have it cascade through all of them to have some final processed output.
 class Blargh():
     # step() gets called by whatever is running the Blargh
     # Subclasses should override this to do whatever needs to be done
@@ -6,6 +9,7 @@ class Blargh():
         # If this is not overriden we should raise an exception
         raise NotImplementedError
 
+# A Blargh that actually contains two blarghs cascaded together.
 class CascadeBlargh(Blargh):
     def __init__(self, b1, b2):
         # Keep track of the two Blarghs we're cascading
--- src/planner/__init__.py
+++ src/planner/__init__.py
@@ -1,5 +1,7 @@
 from blargh import Blargh
 
+# Should eventually take in a target location and plan out a path (waypoints)
+# to be passed to the PID controller
 class PlannerBlargh(Blargh):
     def step(self, inp):
         # TODO: Implement this
