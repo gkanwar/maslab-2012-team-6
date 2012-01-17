3825e65a9761c186c49ad5215511edce7b86a4cc
Gurtej Kanwar <looterguf@HelloThere.(none)>
2012-01-17 16:09:05 -0500

    EVERYTHING

---
+++ src/vision/vision.py
@@ -0,0 +1,23 @@
+from ctypes import cdll
+lib = cdll.LoadLibrary('vision/libfoo.so')
+
+class Vision(object):
+    def __init__(self):
+        self.obj = lib.Foo_new()
+
+    def bar(self):
+        lib.Foo_bar(self.obj)
+    def getNumBalls(self):
+        return lib.Foo_getNumBalls(self.obj)
+    def getR( self, index):
+        return lib.Foo_getR(self.obj, index);
+    def getTheta( self, index):
+        return lib.Foo_getTheta(self.obj, index);
+
+
+
+#f = Foo()
+#f.bar()
+#print f.getNumBalls()
+#print f.getR(0);
+#print f.getTheta(0);
