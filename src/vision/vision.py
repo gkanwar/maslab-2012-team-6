8d89fba77c0689b0ceafa02a14a27ae143969f67
Gurtej Kanwar <looterguf@HelloThere.(none)>
2012-01-20 15:30:12 -0500

    Changes the query frame to python to check speed (it's faster)

--- src/vision/vision.py
+++ src/vision/vision.py
@@ -1,12 +1,21 @@
+import opencv
+import cv
+from opencv import highgui
 from ctypes import cdll
 lib = cdll.LoadLibrary('./libvision.so')
 
 class Vision(object):
     def __init__(self):
-        self.obj = lib.createObj()
+        self.camera = highgui.cvCreateCameraCapture(0)
+        highgui.cvNamedWindow("HelloWorld", cv.CV_WINDOW_AUTOSIZE)
+        #self.obj = lib.createObj()
 
     def processBalls(self):
-        lib.processBalls(self.obj)
+        img = highgui.cvQueryFrame(self.camera)
+        highgui.cvShowImage("HelloWorld", img)
+        highgui.cvWaitKey(10)
+
+    #    lib.processBalls(self.obj)
     #def getNumBalls(self):
     #    return lib.ImageProcessing_getNumBalls(self.obj)
     #def getR(self, index):
