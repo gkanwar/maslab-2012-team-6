df107769fd01a965f653ad4d9ba1ae545b356ac8
Will Oursler <woursler@zeus.(none)>
2012-01-29 15:20:49 -0500

    Makes walls work. Sort of. I'm activly working on the bugs.

--- src/simulator/simulator.py
+++ src/simulator/simulator.py
@@ -1,6 +1,6 @@
 import pygame
 import math
-from math import pi, sin, cos, sqrt, atan2
+from math import pi, sin, cos, sqrt, atan, atan2
 import time
 import random
 
@@ -29,23 +29,30 @@ def toPolar( v ):
 def toVector( ( r, theta ) ):
     return scale( r, unitVector( theta ) )
 
+#Returns an equivalent theta from -pi to pi
+def normalizeTheta( theta ):
+    # Make sure theta is between -pi and pi
+    while theta > pi:
+        theta += -2 * pi
+    while theta < -1 * pi:
+        theta += 2 * pi
+    return theta
 # Actually handles simulating all the objects and the robot
 class Simulator:
     def __init__(self):
         # Initialize variables
         self.size = Vector( 300, 170 )
 
-        self.robot = Robot( scale( .5, self.size ) )
+        self.walls = makeWalls( [ Vector(10,10), Vector(290,10), Vector(290,160), Vector( 10, 160 ), Vector( 10, 10 ) ] )
+        self.robot = Robot( scale( .5, self.size ), 0, self.walls )
         self.balls = [Ball( Vector(random.randint(0, int( self.size.x ) ),
                             random.randint(0, int( self.size.y ) ) ),
                             self.robot)
                       for i in range(12)]
-        
         self.objects = []
         self.objects.extend(self.balls)
+        self.objects.extend(self.walls)
         self.objects.append(self.robot)
-        self.objects.extend( makeWalls( [ Vector(50,40), Vector(30,50), Vector(20,40) ] ) )
-
         # Initialize pygame for visualization
         pygame.init()
         self.screen = pygame.display.set_mode( ( int( PIXELS_PER_INCH * self.size.x ),
@@ -71,12 +78,22 @@ class Simulator:
 
 class Object:
     '''Basic Object Class.'''
-    def __init__(self, position):
+    def __init__(self, position, heading):
         self.position = position
+        self.heading = heading
 
     def setPosition(self, position):
         """ Set the position of the object. """
         self.position = position
+
+    # Takes a vector position and returns the local r theta position with respect to the object.
+    def localize( self, position ):
+        r, theta = toPolar( Vector( position.y - self.position.y, position.x - self.position.x ) )
+
+        #Rotate the point so that theta = 0 means it's right in front of the component.
+        theta = normalizeTheta( theta - self.heading )
+
+        return ( r, theta )
 
     def step(self):
         raise NotImplementedError
@@ -84,7 +101,7 @@ class Object:
     def draw(self, screen):
         raise NotImplementedError
 
-class Component:
+class Component( Object ):
     # Takes a refrence to the robot of which this is a part.
     # offset = offset on the robot in (r, theta) (theta = 0 is the front)
     # headingOffset = Angle the sensor is pointing at in radians.
@@ -102,28 +119,13 @@ class Component:
         self.position = self.robot.position + toVector( ( r, theta + self.robot.heading ) )
 
         self.heading = self.robot.heading + self.headingOffset
-    
-    # Takes a vector object and returns the local r theta position with respect to the object.
-    def localize( self, position ):
-        r, theta = toPolar( Vector( position.y - self.position.y, position.x - self.position.x ) )
-
-        #Rotate the point so that theta = 0 means it's right in front of the component.
-        theta = theta - self.heading
-
-        # Make sure theta is between -pi and pi
-        while theta > pi:
-            theta += -2 * pi
-        while theta < -1 * pi:
-            theta += 2 * pi
-
-        return ( r, theta )
         
 #Encapsulates Camera functionality for nice code later
 class Camera( Component ):
 
     # Takes an vector position (in the world) and returns true if that position can be seen by the camera.
-    def canSee( self, position ):
-        r, theta = self.localize( position )
+    def canSee( self, other ):
+        r, theta = self.localize( other.position )
 
         # Returns True if the position is in the vision cone.
         return ( abs(theta) < CAMERA_MAX_SIGHTING_ANGLE
@@ -138,16 +140,11 @@ class Camera( Component ):
             # (a) the camera is pointing at the ball
             # (b) the ball hasn't been picked up yet.
             # Then we see it!
-            if self.canSee( ball.position ) and not ball.isAquired:
-                r, theta = toPolar( Vector( ball.position.y - self.position.y, ball.position.x - self.position.x ) )
-                theta = self.heading - theta
-                # Make sure theta is between -pi and pi
-                while theta > pi:
-                    theta += -2 * pi
-                while theta < -1 * pi:
-                    theta += 2 * pi
+            if self.canSee( ball ) and not ball.isAquired:
+                r, theta = self.localize( ball.position )
 
-                self.sightedBalls.append( ( r, theta ) )
+                # I think that the convention for VisionBlargh is set opposite the direction here... whoops.
+                self.sightedBalls.append( ( r, -1*theta ) )
                 ball.isSighted = True
             else:
                 ball.isSighted = False
@@ -160,9 +157,11 @@ class Robot(Object):
     way that it can interface with code in the same way as the Arduino
     wrapper(s) and vision module.
     """
-    def __init__(self, position):
+    def __init__(self, position, heading, walls):
         # Initialize position
         self.position = position
+        self.heading = heading
+        self.walls = walls
 
         # Initialize radius
         self.radius = ROBOT_RADIUS
@@ -182,7 +181,6 @@ class Robot(Object):
         # Sensors
         self.camera = Camera( self, ( 0, 0 ), 0 )
         self.components = [ self.camera ]
-        self.sightedBalls = []
 
     # Update the Robot's position
     def step(self):
@@ -192,15 +190,25 @@ class Robot(Object):
 
         # Assuming this function gets called pretty often, it can
         # decouple the motions.
+
+        #Update Position
         avgSpeed = self.maxMotorSpeed * (self.leftMotorSaturation + self.rightMotorSaturation) / 2
         self.position = self.position + scale( delTime * avgSpeed, unitVector( self.heading ) )
 
+        #Update heading
         # That's right. Radians, bitches.
-        self.heading += self.maxMotorSpeed * delTime * (self.leftMotorSaturation - self.rightMotorSaturation) / (4 * self.radius)
-        while self.heading > 2 * pi:
-            self.heading += -2 * pi
-        while self.heading < 0:
-            self.heading += 2 * pi
+        self.heading = normalizeTheta( self.heading + self.maxMotorSpeed * delTime * (self.leftMotorSaturation - self.rightMotorSaturation) / (4 * self.radius) )
+
+        #Check for and fix collisions. Again, this function assumes a frequent call rate. Othewise, it may just miss this.
+        #TODO: check if wall is actually in area...
+        for wall in self.walls:
+            local_wall = localize( wall, self )
+            r, theta, thetaStart, thetaEnd = local_wall
+            overlap = self.radius - r
+            if overlap > 0:
+                print "COLLISION!", overlap
+                #Move the robot so that it's tanget to the wall
+                self.position = self.position - scale( overlap, unitVector( theta + self.heading ) )
 
 	    # Update time
         self.lastTime = currentTime
@@ -262,6 +270,32 @@ class Wall(Object):
                                int(PIXELS_PER_INCH * self.start.y)),
                               (int(PIXELS_PER_INCH * self.end.x),
                                int(PIXELS_PER_INCH * self.end.y)))
+def localize( wall, obj ):
+    # Find the start and end of the lines in local space (x,y)
+    rStart, thetaStart = obj.localize( wall.start )
+    rEnd, thetaEnd = obj.localize( wall.end )
+    start = toVector( (rStart, thetaStart) )
+    end = toVector( (rEnd, thetaEnd) )
+
+    # Calculate the standard form mx+b
+    m = float( start.y - end.y ) / ( start.x - end.x )
+    b = start.y - m * start.x
+    
+    # r = x * cos( theta ) + y * sin( theta )
+    # b = y - mx
+    # Thus assuming b > 0, which we'll cover in a moment ...
+    r = b / sqrt( 1 + m ** 2 )
+    theta = atan( -1.0 / m )
+    
+    #If r < 0, -r -> r, theta + pi -> theta
+    if r < 0:
+        r = -1 * r
+        theta += pi
+    
+    theta = normalizeTheta( theta )
+
+    return( r, theta, thetaStart, thetaEnd )
+    
 class Ball(Object):
     def __init__(self, position, robot):
         # Initialize the location of the ball
